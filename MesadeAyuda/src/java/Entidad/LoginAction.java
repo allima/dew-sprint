598b454821893c1f55cdc10759ec284e63d3714f
riskyglv <riskyglv@c4d98842-00ab-11df-9af4-1984060cff7e>
2010-02-23 03:17:14 +0000

    Ultima actualizacion 20-02-2010

---
+++ MesadeAyuda/src/java/Entidad/LoginAction.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.apache.struts2.ServletActionContext;
+
+/**
+ *
+ * @author rluna
+ */
+public class LoginAction {
+
+    private String UsuarioId;
+    private String Password;
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getUsuarioId() {
+        return UsuarioId;
+    }
+
+    public void setUsuarioId(String UsuarioId) {
+        this.UsuarioId = UsuarioId;
+    }
+
+
+    public String execute() {
+
+        List<Persona> usuario = new ArrayList();
+
+        Persona u = new Persona();
+
+        u.setUsuarioId("palanya");
+        u.setNombres("Pamela Alanya Sánchez");
+        u.setPassword("upc");
+        u.setRol(200);
+        usuario.add(u);
+
+
+
+        u = new Persona();
+        u.setUsuarioId("rluna");
+        u.setNombres("Risky Luna Vega");
+        u.setPassword("upc");
+        u.setRol(100);
+        usuario.add(u);
+
+        System.out.println("Dentro de execute()");
+        System.out.println(UsuarioId);
+        System.out.println(Password);
+
+        String ret="";
+        for (int i = 0; i < usuario.size(); ++i) {
+
+        // Lógica de negocios -> Capa de persistencia
+
+            if (usuario.get(i).getUsuarioId().equals(UsuarioId) && usuario.get(i).getPassword().equals(Password)) {
+                HttpServletRequest request = ServletActionContext.getRequest();
+                HttpSession session = request.getSession();
+                session.setAttribute("USUARIO_ACTUAL", usuario.get(i));
+                System.out.println("Guardando en sesion: " + usuario.get(i).getNombres());
+                ret= "todo_bien";
+                break;
+            } else {
+                ret= "problemas";
+            }
+        }
+        return ret;
+    }
+}
